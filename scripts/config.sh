84af6f0aafb37a8e0e631d9765c16de384ea3f6e
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-08-26 17:42:04 +0100

    Add management scripts for container

---
+++ scripts/config.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Configuration for xtensa-lx106-elf container management scripts
+
+# The name of the container being built
+readonly NAME=${NAME:-'jackwilsdon/xtensa-lx106-elf'}
+
+# The tag of the container being built
+readonly TAG=${TAG:-'0.1.0'}
+
+# The default source directory of the container being built
+readonly DEFAULT_SOURCE='docker'
+
+# The source directory of the container being built
+readonly SOURCE=${SOURCE:-${DEFAULT_SOURCE}}
+
+# The mount directory for the container being built
+readonly MOUNT=${MOUNT:-''}
+
+# Location of the docker binary
+readonly DOCKER=${DOCKER:-$(command -v docker)}
---
+++ scripts/util.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# Utilities for xtensa-lx106-elf container management scripts
+
+# usage: print [args...]
+print() {
+  echo "${CURRENT_SCRIPT}: $*"
+}
+
+# usage: print_raw [args...]
+print_raw() {
+  echo "$*"
+}
+
+# usage: error [args...]
+error() {
+  print "error: $*" >&2
+}
+
+# usage: error_raw [args...]
+error_raw() {
+  print_raw "$*" >&2
+}
+
+# usage: docker [args...]
+run_docker() {
+  "${DOCKER}" "$@"
+
+  local exit_code=$?
+
+  if [[ "${exit_code}" -ne 0 ]]; then
+    error "${DOCKER} exited with code ${exit_code}"
+    return 1
+  fi
+}
+
+# usage: fake_docker [args...]
+fake_docker() {
+  print_raw "${DOCKER}" "$@"
+}
+
+# usage: arg_or_default arg default
+arg_or_default() {
+  if [[ -n "$1" ]]; then
+    echo "$1"
+  else
+    echo "$2"
+  fi
+}

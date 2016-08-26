e8c83e1b2cd5d3d86df83a5f5c3edd3c6a97f6c6
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-08-26 23:29:39 +0100

    Update version to 0.1.1

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,7 +6,7 @@
 readonly NAME=${NAME:-'jackwilsdon/xtensa-lx106-elf'}
 
 # The tag of the container being built
-readonly TAG=${TAG:-'0.1.0'}
+readonly TAG=${TAG:-'0.1.1'}
 
 # The default source directory of the container being built
 readonly DEFAULT_SOURCE='docker'

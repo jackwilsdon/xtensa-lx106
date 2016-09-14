20e285e694c9f8a66bd6a17d1d22b027f8ee101f
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-09-14 22:39:10 +0100

    container -> image

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,13 +2,13 @@
 #
 # Configuration for xtensa-lx106-elf container management scripts
 
-# The default name of the container being built
+# The default name of the image being built
 readonly DEFAULT_NAME='jackwilsdon/xtensa-lx106-elf'
 
-# The default tag of the container being built
+# The default tag of the image being built
 readonly DEFAULT_TAG='0.1.4.dev0'
 
-# The default source directory of the container being built
+# The default source directory of the image being built
 readonly DEFAULT_SOURCE='docker'
 
 # The default location of the docker binary

3406b6e82a864ee20b4b86a0b3c78989854ee380
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-09-24 22:46:52 +0100

    Update version to 0.1.5.dev0

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,7 +6,7 @@
 readonly DEFAULT_NAME='jackwilsdon/xtensa-lx106-elf'
 
 # The default tag of the image being built
-readonly DEFAULT_TAG='0.1.4'
+readonly DEFAULT_TAG='0.1.5.dev0'
 
 # The default source directory of the image being built
 readonly DEFAULT_SOURCE='docker'
